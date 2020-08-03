ef4f970440909a0dd96ab19bf6ed9bb61c39f2e6
Jay Martens <martensjay93@gmail.com>
2020-08-03 13:45:57 -0400

    Created a new route to redirect from the shorturl to the long one

--- express_server.js
+++ express_server.js
@@ -11,8 +11,8 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const generateRandomString =() => {
-
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(7);
 };
 
 app.get("/", (req, res) => {
@@ -37,9 +37,16 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(req.body);  // Log the POST request body to the console  
+  const shortURL = generateRandomString()
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.listen(PORT, () => {
